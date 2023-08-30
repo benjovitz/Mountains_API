06ccab820ce197da1dcbb85b5a9dcc725312d55d
benjovitz <dbenjovitz@gmail.com>
2023-08-30 13:46:42 +0200

    fixed post method

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express")
 const fs = require("fs/promises")
 
 const app = express()
+app.use(express.json())
 
 app.get("/mountains", async (req, res)=>{
     const data = await fs.readFile("./data.json")
@@ -24,14 +25,23 @@ app.get("/mountains/:id", async (req, res)=>{
 })
 
 app.post("/mountains", async (req, res)=>{
+
     const newMountain = req.body
     const data = await fs.readFile("./data.json")
 
     const mountains = JSON.parse(data)
-    console.log(mountains)
     const oldId = mountains[mountains.length - 1].id
+
+    newMountain.id = oldId + 1
     console.log(newMountain)
+
+    mountains.push(newMountain)
+
+    fs.writeFile("./data.json", JSON.stringify(mountains))
+
+    res.sendStatus(200)
 })
+
 
 app.listen(8080)
 
